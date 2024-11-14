48e44a20291d78476f78a1f26962de19a3fc2416
PLUXIN <pluxin485@gmail.com>
2024-11-13 21:28:43 -0300

    test

--- MyApp/src/app/asistencias/asistencias.page.ts
+++ MyApp/src/app/asistencias/asistencias.page.ts
@@ -8,6 +8,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AsistenciasPage implements OnInit {
   registros: any[] = [];
+  totalAsistencias: number = 0;
 
   constructor(private http: HttpClient) {}
 
@@ -20,31 +21,60 @@ export class AsistenciasPage implements OnInit {
     this.http.get<any[]>(`http://localhost:3000/users?username=${username}`).subscribe(users => {
       const user = users[0];
       if (user) {
-        this.http.get<any[]>(`http://localhost:3000/attendances?studentId=${user.id}`).subscribe(attendances => {
-          const registrosMap = new Map();
-          attendances.forEach(attendance => {
-            if (!registrosMap.has(attendance.subject)) {
-              registrosMap.set(attendance.subject, {
-                asignatura: attendance.subject,
-                seccion: attendance.section,
-                clasesAsistidas: 0,
-                totalClases: 0,
-                expandido: false,
-                asistencias: []
+        if (user.role === 'profesor') {
+          this.http.get<any[]>(`http://localhost:3000/attendances?section=${user.section}`).subscribe(attendances => {
+            this.totalAsistencias = attendances.length;
+            const registrosMap = new Map();
+            attendances.forEach(attendance => {
+              if (!registrosMap.has(attendance.subject)) {
+                registrosMap.set(attendance.subject, {
+                  asignatura: attendance.subject,
+                  seccion: attendance.section,
+                  clasesAsistidas: 0,
+                  totalClases: 0,
+                  expandido: false,
+                  asistencias: []
+                });
+              }
+              const registro = registrosMap.get(attendance.subject);
+              registro.totalClases++;
+              if (attendance.presente) {
+                registro.clasesAsistidas++;
+              }
+              registro.asistencias.push({
+                fecha: attendance.date,
+                presente: attendance.presente
               });
-            }
-            const registro = registrosMap.get(attendance.subject);
-            registro.totalClases++;
-            if (attendance.presente) {
-              registro.clasesAsistidas++;
-            }
-            registro.asistencias.push({
-              fecha: attendance.date,
-              presente: attendance.presente
             });
+            this.registros = Array.from(registrosMap.values());
           });
-          this.registros = Array.from(registrosMap.values());
-        });
+        } else {
+          this.http.get<any[]>(`http://localhost:3000/attendances?studentId=${user.id}`).subscribe(attendances => {
+            const registrosMap = new Map();
+            attendances.forEach(attendance => {
+              if (!registrosMap.has(attendance.subject)) {
+                registrosMap.set(attendance.subject, {
+                  asignatura: attendance.subject,
+                  seccion: attendance.section,
+                  clasesAsistidas: 0,
+                  totalClases: 0,
+                  expandido: false,
+                  asistencias: []
+                });
+              }
+              const registro = registrosMap.get(attendance.subject);
+              registro.totalClases++;
+              if (attendance.presente) {
+                registro.clasesAsistidas++;
+              }
+              registro.asistencias.push({
+                fecha: attendance.date,
+                presente: attendance.presente
+              });
+            });
+            this.registros = Array.from(registrosMap.values());
+          });
+        }
       }
     });
   }
